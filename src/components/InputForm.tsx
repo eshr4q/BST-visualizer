45de56335198eb6a02c530e223aca297a086bcc8
eshraq <feshr4q@gmail.com>
2025-03-09 14:45:40 +0330

    inputs logic 1

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,32 +1,51 @@
+import { useState } from "react";
 
 interface InputFormProps {
     onSubmit: (array: number[], target: number) => void;
   }
   
-  const InputForm: React.FC<InputFormProps> = ({ onSubmit }) => {
+  const InputForm: React.FC<InputFormProps> = ({}) => {
+
+    const [numsList, setNumsList] = useState<string>("");
+    const [targetNum, setTargetNum] = useState<string>("");
+
+    const handleSubmit = (e: React.FormEvent) =>{
+      e.preventDefault();
+      console.log("List of Numbers:", numsList);
+      console.log("target Number" , targetNum);
+
+    };
+
     return (
-      <aside className="w-72 p-4 pt-16 bg-base-300 fixed h-screen top-10 left-0 z-0 shadow-md">
-        <form className="space-y-4" onSubmit={(e) => {
-          e.preventDefault();
-          
-          const arrayInput = (e.target as any)[0]?.value.split(',').map(Number) || [];
-          const targetInput = Number((e.target as any)[1]?.value) || 0;
-          onSubmit(arrayInput, targetInput);
-        }}>
+      <aside className="w-full md:w-72 p-4 pt-16 bg-base-300 hidden md:block lg:fixed h-screen top-10 left-0 z-0 shadow-md">
+        <form className="space-y-4" onSubmit={(e) => handleSubmit(e)}>
           <div>
             <label className="input input-info input-md flex items-center gap-2">
               List of Numbers
-              <input type="text" placeholder="e.g., 1,2,3" className="grow" />
+              <input 
+              type="text" 
+              placeholder="e.g., 1,2,3" 
+              className="grow" 
+              value={numsList}
+              onChange={(e)=> setNumsList(e.target.value)} />
             </label>
           </div>
           <div>
             <label className="input input-info input-md flex items-center gap-2">
               Target Number
-              <input type="text" className="grow" placeholder="5" />
+              <input 
+              type="text" 
+              className="grow" 
+              placeholder="5" 
+              value={targetNum}
+              onChange={(e)=> setTargetNum(e.target.value)}/>
               <span className="badge badge-neutral badge-xs">Optional</span>
             </label>
           </div>
-          <button type="submit" className="btn w-full">
+          <button 
+          type="submit" 
+          className="btn w-full"
+          >
             Start Search
           </button>
         </form>
